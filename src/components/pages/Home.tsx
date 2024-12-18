430903f024c5c2c0609e41478bdd92c4bdb6acfc
andrade-fs <andradefs.dev@gmail.com>
2024-12-18 18:59:45 +0100

    [ FIXED ]

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,17 +1,17 @@
 import BookingSteps from "../organs/BookingSteps"
 import HeroSection from "../organs/HeroSection"
 import Maps from "../organs/Maps"
-// import Services from "../organs/Services"
+import Services from "../organs/Services"
 import Testimonials from "../organs/Testimonials"
-import TopDestination from "../organs/TopDestination"
+// import TopDestination from "../organs/TopDestination"
 
 
 const Home = () => {
     return (
         <>
             <HeroSection />
-            {/* <Services /> */}
-            <TopDestination />
+            <Services />
+            {/* <TopDestination /> */}
             <BookingSteps />
             <Testimonials />
             <Maps />
